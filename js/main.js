4c16ddd1f2c2efea5be4d354e55167cd5968bd76
Francesco Melchior <francescomelchior@outlook.com>
2023-05-15 06:51:37 -0300

    tercera version sin alerta, comparte desde el dispositivo

--- js/main.js
+++ js/main.js
@@ -36,21 +36,21 @@ hamburger.addEventListener('click', () => {
 
 
 
+const shareBtn = document.getElementById('shareBtn');
 
-const btnShare = document.querySelector('#btn-share');
-    const linkToCopy = 'www.laspeñascambia2023.com';
-    const alertMessage = document.createElement('div');
-   
-    btnShare.addEventListener('click', () => {
-      navigator.clipboard.writeText(linkToCopy);
-      alertMessage.textContent = 'Enlace copiado en el portapapeles';
-      alertMessage.classList.add('alert');
-      document.body.appendChild(alertMessage);
-      setTimeout(() => {
-        alertMessage.remove();
-      }, 3000);
-    });
-
+shareBtn.addEventListener('click', () => {
+  if (navigator.share) {
+    navigator.share({
+      title: 'Título de tu página',
+      text: 'Texto que se compartirá junto al enlace',
+      url: 'https://www.tupagina.com'
+    })
+      .then(() => console.log('Compartido con éxito'))
+      .catch(error => console.error('Error al compartir:', error));
+  } else {
+    console.log('API de compartir no disponible en este navegador');
+  }
+});
 
 
 
